66f9f47688bb6cfc655fabb3cc95cab13787bc68
Coleman Stewart <colemanstewart17@gmail.com>
2017-09-11 12:31:38 -0700

    Edited API

---
+++ Group15_API/Models/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+namespace Group15_API.Models
+{
+	public abstract class Product:Entity
+	{
+		protected string name;
+		protected double price;
+
+		public Product():base()
+		{
+			name = string.Empty;
+			price = 0;
+		}
+		public Product(int id,string name,double price):base(id)
+		{
+			this.name = name;
+			this.price = price;
+		}
+
+		[XmlAttribute("Name")]
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		[XmlAttribute("Price")]
+		public double Price
+		{
+			get { return price; }
+			set { price = value; }
+		}
+	}
+}
---
+++ Group15_API/Models/Products/PIzza.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+namespace Group15_API.Models.Products
+{
+	public class Pizza : Product
+	{
+		protected string type_field;
+
+		public Pizza() : base()
+		{
+			type_field = string.Empty;
+		}
+		public Pizza(int id,string name,double price,string type):base(id,name,price)
+		{
+			this.type_field = type;
+		}
+
+		[XmlAttribute("Type")]
+		public string Type
+		{
+			get { return type_field; }
+			set { type_field = value; }
+		}
+	}
+}
