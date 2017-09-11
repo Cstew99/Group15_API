c393f7bb72310f6354813403363ef2e77600c247
Coleman Stewart <colemanstewart17@gmail.com>
2017-09-11 11:27:24 -0700

    Create Group API for website

---
+++ Group15_API/Models/Entity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+namespace Group15_API.Models
+{
+	[XmlRoot("Entity")]
+	public abstract class Entity
+	{
+		protected int id;
+
+		public Entity()
+		{
+			id = 0;
+		}
+		public Entity(int id)
+		{
+			this.id = id;
+		}
+
+		[XmlAttribute("ID")]
+		public int ID
+		{
+			get { return id;}
+			set { id = value; }
+		}
+	}
+}
