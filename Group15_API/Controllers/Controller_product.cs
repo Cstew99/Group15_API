24cf8e455bb8c159878fdc793b1b1526011731b9
Coleman Stewart <colemanstewart17@gmail.com>
2017-09-11 12:50:13 -0700

    Edited

---
+++ Group15_API/Controllers/Controller_product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group15_API.Controllers
+{
+	public class Controller_product : Controller<Models.Product>
+	{
+		public string[] Product_Cards(Type type)
+		{
+			try
+			{
+				var list = new List<string>();
+				foreach (Models.Product product in collection)
+				{
+					if (product.GetType()==type)
+					{
+						list.Add(product.To_HTMLCard());
+					}
+				}
+				return list.ToArray();
+			} catch { return null; }
+		}
+	}
+}
